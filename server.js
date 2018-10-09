8714669a68d02d93e84ac8d5fdb9e0d7504819a1
AggressivelyMeows <cerulean.connor@gmail.com>
2018-10-09 18:28:57 +0100

    Added S3 support. Fixed critical bug in which the server shuts down

--- server.js
+++ server.js
@@ -7,14 +7,28 @@ const Grid = require('mongodb').GridFSBucket;
 // QB V0.7 CDN for images
 // can be used as a slide in replacement for any CDN url
 
-// add "cdn_url": "http://localhost:8086/" to config.json to route all
+// add "cdn_url": "http://localhost:8086" to config.json to route all
 // image requests to this CDN
 
 var db, gridFS
 
+app.get('/*', function (req, res, next) {
+    res.header('X-CDN-TYPE', 'Vesion 0.1 (https://github.com/AggressivelyMeows/QuartzBoardCDN)');
+    next(); // http://expressjs.com/guide.html#passing-route control
+});
+
 app.get('/api/image/:fileID', function (req, res) {
-    res.set('Content-Type', 'image/png');
-    gridFS.openDownloadStream(req.params.fileID).pipe(res);
+    console.log(db)
+    db.collection('images').findOne({ fileID: req.params.fileID }, (err, result) => {
+        res.set('Content-Type', 'image/png');
+        if (result.location == 'gridFS') {
+            gridFS.openDownloadStream(req.params.fileID).pipe(res);
+        } else {
+            res.redirect(`https://quartz.nyc3.digitaloceanspaces.com/${result.userID}/${result.fileID}`)
+        }
+    })
+
+    
 });
 
 app.get('/api/image/:fileID/thumbnail', function (req, res) {
@@ -27,6 +41,6 @@ MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     db = client.db('quartz');
     gridFS = new Grid(client.db('image_storage'));
     app.listen(8086, () => {
-        console.log('QuartzBoard Image CDN.\nListening on port 8086.\nEnsure you have set the following inside of your config.json:\n' + JSON.stringify({cdn_url: 'http://<your-server-location>:8086/'}));
+        console.log('QuartzBoard Image CDN.\nListening on port 8086.\nEnsure you have set the following inside of your config.json:\n' + JSON.stringify({cdn_url: 'http://<your-server-location>:8086'}));
     })
 })
