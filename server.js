652b6372be527b348c02af2670020d174b8c6c6e
AggressivelyMeows <cerulean.connor@gmail.com>
2018-10-10 23:45:22 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
+`
+MIT License
+
+Copyright (c) 2018 Cerulean (CeruTech)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+`
 const express = require('express');
 const app = express();
 const responseTime = require('response-time');
@@ -5,12 +28,12 @@ app.use(responseTime());
 const grid = require("gridfs-stream");
 const MongoClient = require('mongodb').MongoClient
 const Grid = require('mongodb').GridFSBucket;
+const logger = require('pino')()
 
 const sharp = require('sharp'); // for image optimisation
 
-// QB V0.7 CDN for images
+// QuartzBoard Image CDN - Made by CeruTech
 // can be used as a slide in replacement for any CDN url
-
 // add "cdn_url": "http://localhost:8086" to config.json to route all
 // image requests to this CDN
 
@@ -19,20 +42,17 @@ var supportedFormats = {
     png: { 'mimetype': 'image/png' },
     webp: { 'mimetype': 'image/webp' },
     jpg: {'mimetype': 'image/jpg'}
-
 }
 
-function reformat(data, format = 'png') {
-    
+function reformat(data, format = 'png', quality=100) {
     var transformer = sharp();
-    transformer = transformer.toFormat(format);
-    
+    transformer = transformer.toFormat(format, { quality: quality });
     return data.pipe(transformer)
-
 }
 
 app.get('/*', function (req, res, next) {
     res.header('X-CDN-TYPE', 'Vesion 0.1 (https://github.com/AggressivelyMeows/QuartzBoardCDN)');
+    logger.debug(`${req.url} - ${req.ip}`)
     next(); // http://expressjs.com/guide.html#passing-route control
 });
 
@@ -40,7 +60,7 @@ app.get('/api/image/:fileID', function (req, res) {
     db.collection('images').findOne({ fileID: req.params.fileID }, (err, result) => {
         
         res.set('Cache-Control', 'public, max-age=604800'); // 1 week
-
+        
         if (result.location == 'gridFS') {
             if (req.query.format) {
                 if (!(req.query.format in supportedFormats)) {
@@ -71,7 +91,22 @@ app.get('/api/image/:fileID/thumbnail', function (req, res) {
         res.set('Content-Type', 'image/png');
         res.set('Cache-Control', 'public, max-age=604800'); // 1 week
         if (result.location == 'gridFS') {
-            gridFS.openDownloadStream(fID + '_thumbnail').pipe(res);
+            if (req.query.format) {
+                if (!(req.query.format in supportedFormats)) {
+                    res.send({ 'error': 'Format is not supported.' })
+                } else {
+                    // format is supported
+                    // convert.
+                    var readStream = gridFS.openDownloadStream(req.params.fileID + '_thumbnail');
+                    res.set('Content-Type', supportedFormats[req.query.format].mimetype);
+                    return reformat(readStream, format = req.query.format).pipe(res);
+                }
+            } else {
+                // no format change, render the pure image from the database
+                res.set('Content-Type', 'image/png');
+                gridFS.openDownloadStream(req.params.fileID + '_thumbnail').pipe(res);
+            }
+
         } else {
             res.redirect(`https://quartz.nyc3.cdn.digitaloceanspaces.com/${result.userID}/${fID}_thumbnail.png`);
         };
