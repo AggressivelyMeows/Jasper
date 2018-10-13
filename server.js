97d8434b3bf9cf9ade0b1a734456498285eff87c
AggressivelyMeows <cerulean.connor@gmail.com>
2018-10-13 21:47:05 +0100

    Added cache [more in desc]
    Added a way for images to be stored inside the database so that they do not need to be re-rendered. This takes the time to serve a webp from 400ms to 20ms. Speed. Im also adding a new type of object called a history or audit log, still working on the name.

--- server.js
+++ server.js
@@ -24,25 +24,26 @@ SOFTWARE.
 const express = require('express');
 const app = express();
 const responseTime = require('response-time');
+var cors = require('cors')
 app.use(responseTime());
+app.use(cors());
+app.options('*', cors());
+
 const grid = require("gridfs-stream");
 const MongoClient = require('mongodb').MongoClient
 const Grid = require('mongodb').GridFSBucket;
-const logger = require('pino')()
-
 const sharp = require('sharp'); // for image optimisation
 
-// QuartzBoard Image CDN - Made by CeruTech
+// Jasper CDN - Made by CeruTech
 // can be used as a slide in replacement for any CDN url
-// add "cdn_url": "http://localhost:8086" to config.json to route all
+// add "cdn_url": "http://<your-server-location>:8086" to config.json to route all
 // image requests to this CDN
 
 var db, gridFS
 var supportedFormats = {
     png: { 'mimetype': 'image/png' },
-    webp: { 'mimetype': 'image/webp' },
-    jpg: {'mimetype': 'image/jpg'}
-}
+    webp: { 'mimetype': 'image/webp' }
+} // removed support for JPEG. like seriously, why do people still want JPEG?????
 
 function reformat(data, format = 'png', quality=100) {
     var transformer = sharp();
@@ -51,8 +52,7 @@ function reformat(data, format = 'png', quality=100) {
 }
 
 app.get('/*', function (req, res, next) {
-    res.header('X-CDN-TYPE', 'Vesion 0.1 (https://github.com/AggressivelyMeows/QuartzBoardCDN)');
-    logger.debug(`${req.url} - ${req.ip}`)
+    res.header('X-CDN-TYPE', 'Vesion 0.6 (https://github.com/AggressivelyMeows/Jasper)');
     next(); // http://expressjs.com/guide.html#passing-route control
 });
 
@@ -67,10 +67,37 @@ app.get('/api/image/:fileID', function (req, res) {
                     res.send({ 'error': 'Format is not supported.' })
                 } else {
                     // format is supported
-                    // convert.
-                    var readStream = gridFS.openDownloadStream(req.params.fileID);
-                    res.set('Content-Type', supportedFormats[req.query.format].mimetype);
-                    return reformat(readStream, format = req.query.format).pipe(res);
+                    // convert and save to DB for future use
+                    var format = req.query.format.toLowerCase();
+                    var fileID = req.params.fileID + `_${format}`;
+                    if (!format.length > 1) {
+                        format = 'png'; //default back to png if no format detected
+                    }
+                    if (format == 'png') {
+                        fileID = req.params.fileID;
+                    }
+                    if (!('formats' in result)) {
+                        console.log('heck')
+                        // object does not support formats
+                        // try to add a polyfill
+                        result.formats = [];
+                    }
+                    if (result.formats.includes(format) || format == 'png') {
+                        // we have the file ready to go!
+                        var readStream = gridFS.openDownloadStream(fileID);
+                        res.set('Content-Type', supportedFormats[format].mimetype);
+                        readStream.pipe(res);
+                    } else {
+                        // generate the file and send back, saving the new version in the database
+                        var readStream = gridFS.openDownloadStream(req.params.fileID); // just get the default image
+                        res.set('Content-Type', supportedFormats[format].mimetype);
+                        res.set('X-Explain', `Rendered new copy of image in ${format}`);
+                        reformat(readStream, format = req.query.format).pipe(res);
+                        // send the file to the database now
+                        db.collection('images').findOneAndUpdate({ fileID: req.params.fileID }, { $push: { 'formats': format, 'history': `[Jasper] Created copy of image in ${format}`} });
+                        var writeStream = gridFS.openUploadStreamWithId(req.params.fileID + '_' + format, req.params.fileID + '_' + format );
+                        reformat(readStream, format = req.query.format).pipe(writeStream);
+                    }
                 }
             } else {
                 // no format change, render the pure image from the database
@@ -112,6 +139,7 @@ app.get('/api/image/:fileID/thumbnail', function (req, res) {
         };
     })
 });
+
 
 MongoClient.connect('mongodb://localhost:27017', (err, client) => {
     if (err) return console.log(err);
